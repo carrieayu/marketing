92e5d34fa3b237ab8a2b357127f56c2ace43d643
Peter Aliasut <peterneil0312@gmail.com>
2022-05-11 15:29:28 +0800

    Good day yuyu this is new

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -3,10 +3,9 @@ export const Services = (props) => {
     <div id='services' className='text-center'>
       <div className='container'>
         <div className='section-title'>
-          <h2>Our Services</h2>
+          <h2>Our Development Technologies</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            Only the most in demand tools for today’s Web Development and Design.
           </p>
         </div>
         <div className='row'>
